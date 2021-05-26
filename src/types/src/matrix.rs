0e646a722426ff05396e86684d06e806f0e18f7e
fhools <fhools@gmail.com>
2021-05-25 22:56:48 -0700

    matrix eq, mul

--- src/types/src/matrix.rs
+++ src/types/src/matrix.rs
@@ -1,3 +1,6 @@
+use utils::*;
+
+#[derive(Debug)]
 pub struct Matrix4x4 {
     pub m: [[f64;4]; 4]
 }
@@ -12,5 +15,164 @@ impl Matrix4x4 {
         m.m[2][2] = 1.0;
         m.m[3][3] = 1.0;
         m
+    }
+
+
+    pub fn from_vectors(r1: &[f64], r2: &[f64], r3: &[f64], r4: &[f64]) -> Matrix4x4 {
+        let mut m = Matrix4x4 {
+            m: [[0.0; 4]; 4]
+        };
+
+        for i in 0..4 {
+            m.m[0][i] = r1[i];
+        }
+        for i in 0..4 {
+            m.m[1][i] = r2[i];
+        }
+        for i in 0..4 {
+            m.m[2][i] = r3[i];
+        }
+        for i in 0..4 {
+            m.m[3][i] = r4[i];
+        }
+
+        m
+    }
+
+    pub fn eq(&self, other: &Matrix4x4) -> bool {
+        for i in 0..4 {
+            for j in 0..4 {
+                if !f64_eq(self.m[i][j], other.m[i][j]) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    
+    pub fn mul(&self, other: &Matrix4x4) -> Matrix4x4 {
+        let mut prod = Matrix4x4::new();
+        for r in 0..4 {
+            for c in 0..4 {
+                let dot = self.m[r][0] * other.m[0][c] +
+                    self.m[r][1] * other.m[1][c] +
+                    self.m[r][2] * other.m[2][c] +
+                    self.m[r][3] * other.m[3][c];
+                prod.m[r][c] = dot;
+            }
+        }
+        prod
+    }
+}
+
+#[derive(Debug)]
+pub struct Matrix3x3 {
+    pub m: [[f64; 3]; 3]
+}
+
+impl Matrix3x3 {
+    pub fn new() -> Matrix3x3 {
+        let mut m = Matrix3x3 {
+            m: [[0.0; 3]; 3]
+        };
+        m.m[0][0] = 1.0;
+        m.m[1][1] = 1.0;
+        m.m[2][2] = 1.0;
+        m
+    }
+
+    pub fn from_vectors(r1: &[f64], r2: &[f64], r3: &[f64]) -> Matrix3x3 {
+        let mut m = Matrix3x3 {
+            m: [[0.0; 3]; 3]
+        };
+
+        for i in 0..3 {
+            m.m[0][i] = r1[i];
+        }
+        for i in 0..3 {
+            m.m[1][i] = r2[i];
+        }
+        for i in 0..3 {
+            m.m[2][i] = r3[i];
+        }
+        m
+    }
+
+    pub fn eq(&self, other: &Matrix3x3) -> bool {
+        for i in 0..3 {
+            for j in 0..3 {
+                if !f64_eq(self.m[i][j], other.m[i][j]) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    pub fn mul(&self, other: &Matrix3x3) -> Matrix3x3 {
+        let mut prod = Matrix3x3::new();
+        for r in 0..3 {
+            for c in 0..3 {
+                let dot = self.m[r][0] * other.m[0][c] +
+                    self.m[r][1] * other.m[1][c] +
+                    self.m[r][2] * other.m[2][c];
+                prod.m[r][c] = dot;
+            }
+        }
+        prod
+    }
+}
+
+
+#[derive(Debug)]
+pub struct Matrix2x2 {
+    pub m: [[f64;2]; 2]
+}
+
+impl Matrix2x2 {
+    pub fn new() -> Matrix2x2 {
+        let mut m = Matrix2x2 {
+            m: [[0.0; 2]; 2]
+        };
+        m.m[0][0] = 1.0;
+        m.m[1][1] = 1.0;
+        m
+    }
+
+    pub fn from_vectors(r1: &[f64], r2: &[f64]) -> Matrix2x2 {
+        let mut m = Matrix2x2 {
+            m: [[0.0; 2]; 2]
+        };
+
+        for i in 0..2 {
+            m.m[0][i] = r1[i];
+        }
+        for i in 0..2 {
+            m.m[1][i] = r2[i];
+        }
+        m
+    }
+
+    pub fn eq(&self, other: &Matrix2x2) -> bool {
+        for i in 0..2 {
+            for j in 0..2 {
+                if !f64_eq(self.m[i][j], other.m[i][j]) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    pub fn mul(&self, other: &Matrix2x2) -> Matrix2x2 {
+        let mut prod = Matrix2x2::new();
+        for r in 0..2 {
+            for c in 0..2 {
+                let dot = self.m[r][0] * other.m[0][c] +
+                    self.m[r][1] * other.m[1][c];
+                prod.m[r][c] = dot;
+            }
+        }
+        prod
     }
 }
--- tests/ch3.rs
+++ tests/ch3.rs
@@ -7,3 +7,82 @@ fn matrix4x4_new() {
     let m = Matrix4x4::new();
     assert!(m.m[0][0] == 1.0);
 }
+
+#[test]
+fn matrix4x4_from_vec() {
+    let r1 = vec![1.0, 2.0, 3.0, 4.0];
+    let r2 = vec![5.0, 2.0, 9.0, 6.0];
+    let r3 = vec![9.0, 8.0, 5.0, 1.0];
+    let r4 = vec![4.0, 3.0, 11.0, 10.0];
+    let m = Matrix4x4::from_vectors(&r1, &r2, &r3, &r4);
+    assert!(f64_eq(m.m[0][0], 1.0));
+    assert!(f64_eq(m.m[1][0], 5.0));
+    assert!(f64_eq(m.m[2][0], 9.0));
+    assert!(f64_eq(m.m[3][0], 4.0));
+}
+
+
+#[test]
+fn matrix3x3_from_vec() {
+    let r1 = vec![1.0, 2.0, 3.0];
+    let r2 = vec![5.0, 2.0, 9.0];
+    let r3 = vec![9.0, 8.0, 5.0];
+    let m = Matrix3x3::from_vectors(&r1, &r2, &r3);
+    assert!(f64_eq(m.m[0][0], 1.0));
+    assert!(f64_eq(m.m[1][0], 5.0));
+    assert!(f64_eq(m.m[2][0], 9.0));
+}
+
+#[test]
+fn matrix2x2_from_vec() {
+    let r1 = vec![1.0, 2.0];
+    let r2 = vec![5.0, 2.0];
+    let m = Matrix2x2::from_vectors(&r1, &r2);
+    assert!(f64_eq(m.m[0][0], 1.0));
+    assert!(f64_eq(m.m[1][0], 5.0));
+}
+
+#[test]
+fn matrix2x2_mul() {
+    let r1 = vec![1.0, 2.0];
+    let r2 = vec![5.0, 2.0];
+    let m = Matrix2x2::from_vectors(&r1, &r2);
+    let n = Matrix2x2::from_vectors(&r1, &r2);
+    let p = m.mul(&n);
+
+    println!("p: {:?}", p);
+    assert!(f64_eq(p.m[0][0], 11.0));
+    assert!(f64_eq(p.m[0][1], 6.0));
+    assert!(f64_eq(p.m[1][0], 5.0 + 10.0));
+    assert!(f64_eq(p.m[1][1], 10.0 + 4.0));
+}
+
+#[test]
+fn matrix_eq() {
+    let m = Matrix4x4::new();
+    let n = Matrix4x4::new();
+
+    assert!(m.eq(&n));
+    
+    let o = Matrix3x3::new();
+    let p = Matrix3x3::new();
+    assert!(p.eq(&o));
+
+    let q = Matrix3x3::new();
+    let r = Matrix3x3::new();
+    assert!(q.eq(&r));
+
+    let mut s = Matrix3x3::new();
+    s.m[0][0] = 2.0;
+
+    let t = Matrix3x3::new();
+    assert!(!s.eq(&t));
+    assert!(!t.eq(&s));
+
+
+
+
+}
+
+
+
